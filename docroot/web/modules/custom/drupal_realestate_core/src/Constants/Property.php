20bf4ed47866f2e35fa3813ed82e10c7261c3caf
Aldrin Thomas Labarda <aldrin.thomas.labarda@codeandtheory.com>
2020-01-13 17:57:04 +0800

    DPL-945: Created web app constants and core module info yml

---
+++ docroot/web/modules/custom/drupal_realestate_core/src/Constants/Property.php
@@ -0,0 +1,11 @@
+<?php
+namespace Drupal\drupal_realestate_core\Constants;
+
+class Property {
+  /**
+   * Property Types.
+   */
+  public const PROPERTY_TYPE_RENT     = 'property_rent';
+
+  public const PROEPRTY_TYPE_PURCHASE = 'property_purchase';
+}
